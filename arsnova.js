ff6fc161e2d66f1091ec7c20dffd3ae93c0572d3
Linda Fabian <linda.fabian@mni.thm.de>
2016-01-21 17:23:15 +0100

    test xmlhttprequest

--- arsnova.js
+++ arsnova.js
@@ -1,1 +1,20 @@
 alert("Hallooo");
+
+var xmlHttpObject = new XMLHttpRequest();
+
+    // Funktion, die bei Statusänderungen reagiert
+    function handleStateChange()
+    {
+        // Derzeitigen Status zurückgeben
+        alert("xmlHttpObject.readyState = " + xmlHttpObject.readyState + (xmlHttpObject.readyState >= 3 ? " HTTP-Status = " + xmlHttpObject.status : ''));
+    }
+    
+    // Wenn Dokument geladen ausführen
+    window.onload = function() {
+        // Anfrage vorbereiten, ruft www.webmasterpro.de auf
+        xmlHttpObject.open('GET', 'https://arsnova.eu/api/statistics/');
+        // Handler hinterlegen
+        xmlHttpObject.onreadystatechange = handleStateChange;
+        // Anfrage abschicken
+        xmlHttpObject.send(null);
+    }
