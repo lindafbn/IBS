e86486d5393013975435624b207e54943c9919f2
Linda Fabian <linda.fabian@mni.thm.de>
2016-01-21 17:13:27 +0100

    test javascript hello

--- js/arsnova.js
+++ js/arsnova.js
@@ -1,18 +1,1 @@
-var xmlhttp = null;
-if(window.XMLHttpRequest){        // Firefox, Safari, ... 
-   xmlhttp = new XMLHttpRequest();
-} else if(window.ActiveXObject){  // IE Windows
-   xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-}
-if (xmlhttp) {
-   xmlhttp.open("HEAD", "jsBeispiele/xml/data.xml", true);
-   xmlhttp.onreadystatechange=function() {
-      if (xmlhttp.readyState==4) {
-         var res = document.createElement('pre');
-         var resText = document.createTextNode(xmlhttp.getAllResponseHeaders());
-         res.appendChild(resText);
-         httpHead.appendChild(res);
-      }
-   }
-   xmlhttp.send(null);
-}
+alert("Halloooo");
